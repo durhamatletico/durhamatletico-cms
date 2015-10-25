b9c3ff3b4beab1560b80982c8e849a52f2df347a
Kosta Harlan <kosta@kostaharlan.net>
2015-10-24 20:50:56 -0400

    Add trusted host settings, Drupal code standards

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -1,14 +1,16 @@
 <?php
 
 /**
+ * @file
  * Load services definition file.
  */
+
 $settings['container_yamls'][] = __DIR__ . '/services.yml';
 
 /**
  * Include the Pantheon-specific settings file.
  *
- * n.b. The settings.pantheon.php file makes some changes
+ * N.B. The settings.pantheon.php file makes some changes
  *      that affect all envrionments that this site
  *      exists in.  Always include this file, even in
  *      a local development environment, to insure that
@@ -17,30 +19,36 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
 include __DIR__ . "/settings.pantheon.php";
 
 /**
- * If there is a local settings file, then include it
+ * If there is a local settings file, then include it.
  */
 $local_settings = __DIR__ . "/settings.local.php";
+
 if (file_exists($local_settings)) {
   include $local_settings;
- }
+}
+
 $settings['install_profile'] = 'standard';
 if (isset($_ENV['PANTHEON_ENVIRONMENT'])) {
   if ($_ENV['PANTHEON_ENVIRONMENT'] === 'dev') {
     $domain = 'dev.durhamatletico.com';
+    $settings['trusted_host_patterns'] = array(
+      '^dev\.durhamatletico\.com$',
+    );
   }
   if ($_ENV['PANTHEON_ENVIRONMENT'] === 'test') {
     $domain = 'test.durhamatletico.com';
+    $settings['trusted_host_patterns'] = array(
+      '^test\.durhamatletico\.com$',
+    );
   }
   if ($_ENV['PANTHEON_ENVIRONMENT'] === 'live') {
     $domain = 'www.durhamatletico.com';
+    $settings['trusted_host_patterns'] = array(
+      '^www\.durhamatletico\.com$',
+    );
   }
   else {
-    # Fallback value for multidev or other environments.
-    # This covers environment-sitename.pantheon.io domains
-    # that are generated per environment.
     $domain = $_SERVER['HTTP_HOST'];
   }
-
-  # This global variable determines the base for all URLs in Drupal.
-  $base_url = 'https://'. $domain;
+  $base_url = 'https://' . $domain;
 }
