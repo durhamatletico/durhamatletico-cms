c5ddcd5ce2b688f630ffa06e5fed6f499019cd25
Kosta Harlan <kosta@kostaharlan.net>
2015-11-07 10:59:53 -0500

    Initial commit of registration module

---
+++ modules/custom/durhamatletico_registration/src/RegistrationService.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\durhamatletico_registration\RegistrationService.
+ */
+
+namespace Drupal\durhamatletico_registration;
+
+
+/**
+ * Class RegistrationService.
+ *
+ * @package Drupal\durhamatletico_registration
+ */
+class RegistrationService implements RegistrationServiceInterface {
+  /**
+   * Constructor.
+   */
+  public function __construct() {
+
+  }
+
+}
---
+++ modules/custom/durhamatletico_registration/src/RegistrationServiceInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\durhamatletico_registration\RegistrationServiceInterface.
+ */
+
+namespace Drupal\durhamatletico_registration;
+
+/**
+ * Interface RegistrationServiceInterface.
+ *
+ * @package Drupal\durhamatletico_registration
+ */
+interface RegistrationServiceInterface {
+
+
+}
