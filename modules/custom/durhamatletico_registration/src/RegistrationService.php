3169a623ae4fbf0ab586246c31f78c96f03b4ba8
Kosta Harlan <kosta@kostaharlan.net>
2015-11-07 16:30:06 -0500

    Work in progress

---
+++ modules/custom/durhamatletico_registration/src/RegistrationService.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\durhamatletico_registration\RegistrationService.
+ */
+
+namespace Drupal\durhamatletico_registration;
+
+/**
+ * Class RegistrationService.
+ *
+ * @package Drupal\durhamatletico_registration
+ */
+class RegistrationService {
+  /**
+   * Constructor.
+   */
+  public function __construct() {
+
+  }
+
+}
+
