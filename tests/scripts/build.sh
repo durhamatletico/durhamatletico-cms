2e2e29d2371c47446df96938e900ba4a6aa32263
Kosta Harlan <kosta@kostaharlan.net>
2015-11-08 11:01:27 -0500

    Add automated testing configuration
    - Add a Circle.yml file
    - Behat configuration
    - Scripts for building on CircleCI
    
    Fixes #15.

---
+++ tests/scripts/build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+# Get Pantheon's Command Line Tool, terminus.
+sudo curl https://github.com/pantheon-systems/cli/releases/download/0.9.2/terminus.phar -L -o /usr/local/bin/terminus && sudo chmod +x /usr/local/bin/terminus
+
+# Log into terminus.
+terminus auth login $PANTHEON_EMAIL --password=$PANTHEON_PASSWORD
+
+# Get a dump from production
+terminus site backups get --element=db --site=durham-atletico --env=live --to=database.sql.gz --latest
+
+# Import the DB
+echo "CREATE database durhamatletico_circle;" | mysql -uroot
+gunzip < database.sql.gz | mysql -uroot durhamatletico_circle
+
+# TODO: Get files?
+
+# Install Drush
+sudo curl https://github.com/drush-ops/drush/releases/download/8.0.0-rc3/drush.phar -L -o /usr/local/bin/drush && sudo chmod +x /usr/local/bin/drush
+
+# Overwrite settings.local.php
+sudo mv tests/scripts/settings.local.php sites/default/settings.local.php
+
+# Clear cache
+drush cr -yv
+drush config-import -yv
+
+drush runserver --server=builtin --strict=0 </dev/null &>$HOME/server.log &
+
+# Copy Behat local config.
+cp tests/scripts/behat.local.yml tests/behat.local.yml
+
+echo "Ready for testing!"
---
+++ tests/scripts/settings.local.php
@@ -0,0 +1,14 @@
+<?php
+
+$databases['default']['default'] = array(
+  'database' => 'durhamatletico_circle',
+  'username' => 'root',
+  'password' => '',
+  'prefix' => '',
+  'host' => '127.0.0.1',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+
+$settings['hash_salt'] = 'abc';
