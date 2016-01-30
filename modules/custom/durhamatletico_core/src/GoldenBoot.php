007d9cf78cf538f6e574812c7e70f54c3c376340
Kosta Harlan <kosta@kostaharlan.net>
2016-01-30 11:32:16 -0500

    Initial work on golden boot

---
+++ modules/custom/durhamatletico_core/src/GoldenBoot.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\durhamatletico_core\GoldenBoot.
+ */
+
+namespace Drupal\durhamatletico_core;
+
+
+/**
+ * Class GoldenBoot.
+ *
+ * @package Drupal\durhamatletico_core
+ */
+class GoldenBoot implements GoldenBootInterface {
+  /**
+   * Constructor.
+   */
+  public function __construct() {
+
+  }
+
+}
