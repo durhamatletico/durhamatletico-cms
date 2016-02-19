d889227843d1b965bd16329327e45edbe67c61e1
Kosta Harlan <kosta@kostaharlan.net>
2016-02-19 15:46:08 -0500

    Add production deployment script

---
+++ private/scripts/deploy-production.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# exit immediately if any command fails
+set -e
+
+terminus site backups create --env=live --element=all
+terminus site deploy --env=live --updatedb --cc
+terminus drush cim -y --env=live
