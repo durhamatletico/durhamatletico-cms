7899249008698de767e3f47075adff7a5c45d840
Kosta Harlan <kosta@kostaharlan.net>
2016-02-26 14:19:10 -0500

    Fix bracket display, for now

--- themes/atletico/js/global.js
+++ themes/atletico/js/global.js
@@ -20,25 +20,28 @@
             url: "/api/tournaments/winter2016/".concat(value)
         }).then(function(data) {
             var teams = [];
-            var results = [];
+            var round_one_results = [];
             $.each(data, function (index, value) {
                 teams.push([value.home, value.away]);
-                results.push([value.home_score, value.away_score]);
+                round_one_results.push([value.home_score, value.away_score]);
             });
+
+            for (var i = 0; i < round_one_results.length; i++) {
+                for (var x = 0; x < round_one_results[i].length; x++) {
+                    round_one_results[i][x] = parseInt(round_one_results[i][x]);
+                }
+            };
             var data = {
                 teams : teams,
                 results :
                     [
-                        [
-                        ],
-                        [
-                            [
-                            ]
-                        ]
+                        round_one_results,
+                        [[], []],
+                        [[]]
                     ]
             };
             $('#'.concat(value)).bracket({
-                init: data,
+                init: data
             });
         });
 
