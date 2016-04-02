89e2f5e2f68c8cc6b18e05127b3777603260aa60
Kosta Harlan <kosta@kostaharlan.net>
2016-04-01 22:38:50 -0400

    Initial pass at cleaning up Stripe

--- modules/contrib/stripe_checkout/src/StripeCallback.php
+++ modules/contrib/stripe_checkout/src/StripeCallback.php
@@ -9,8 +9,11 @@ namespace Drupal\stripe_checkout;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\node\Entity\Node;
+use Stripe\Charge;
+use Stripe\Stripe;
+use Stripe\Error\Card;
 
-require_once(__DIR__ . '/../vendor/autoload.php');
+require_once __DIR__ . '/../vendor/autoload.php';
 
 class StripeCallback extends ControllerBase {
 
@@ -19,16 +22,15 @@ class StripeCallback extends ControllerBase {
     // Load the API key
     $config = \Drupal::config('stripe_checkout.settings');
     $skey = $config->get('stripe_checkout_key_secret');
-    \Stripe\Stripe::setApiKey($skey);
+    Stripe::setApiKey($skey);
     $default_currency = $config->get('stripe_checkout_currency');
+    // Receive the token from Stripe.
+    $token = \Drupal::request()->request->get('stripeToken');
 
-    // Receive the token from Stripe
-    $token = $_POST['stripeToken'];
-
-    // Load the node to get the correct values
+    // Load the node to get the correct values.
     $node = Node::load($id);
 
-    // Find the field that was used for creating the charge
+    // Find the field that was used for creating the charge.
     $view_mode = \Drupal::config('core.entity_view_display.node.' . $node->getType() . '.default');
     $fields = $view_mode->get('content');
     foreach ($fields as $field => $values) {
@@ -43,29 +45,30 @@ class StripeCallback extends ControllerBase {
       }
     }
 
-    // Process the charge
-    $charge = \Stripe\Charge::create(array(
-      "amount" => $node->$clicked->value,
-      "currency" => $currency,
-      "source" => $token,
-      "description" => $node->title->value,
-    ));
-
-    if ($charge->paid === true) {
+    // Process the charge.
+    try {
+      $charge = Charge::create(array(
+        "amount" => $node->$clicked->value,
+        "currency" => $currency,
+        "source" => $token,
+        "description" => $node->title->value,
+        "metadata" => array("registration_nid" => $id),
+      ));
       drupal_set_message(t("Thank you. Payment has been processed."));
-      // Update the node to mark as paid
+      // Update the node to mark as paid.
       $node->$clicked->value = 0;
-
-      $node->revision = new stdClass();
-      $node->revision->value = true;
-      $node->revision_log->value = "Payment: " . $charge->id; // Messy, but it works.
+      $node->revision = new \stdClass();
+      $node->revision->value = TRUE;
+      $node->revision_log->value = "Payment: " . $charge->id;
       $node->save();
     }
-    else {
+    catch (Card $e) {
+      \Drupal::logger('stripe_checkout')->error('Payment failed registration node ' . (int) $id);
       drupal_set_message(t("Payment failed."), 'error');
     }
 
-    // Go back to the node
+    // Go back to the node.
+    // TODO: Redirect to user page.
     return $this->redirect('entity.node.canonical', array('node' => $id));
   }
 }
