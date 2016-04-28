b54c9a42baca21dae6f99d1afda3d87e640711a5
Kosta Harlan <kosta@kostaharlan.net>
2016-04-28 15:25:38 -0400

    Add standings and previous matches blocks

--- private/scripts/refresh-local.sh
+++ private/scripts/refresh-local.sh
@@ -8,6 +8,5 @@ echo "Downloading backup"
 terminus site backups get --element=db --site=durham-atletico --env=live --to=database.sql.gz --latest
 rm database.sql
 echo "y" | gunzip database.sql.gz
-export VAGRANT_CWD=/home/kosta/src/drupal-vm
 echo "Importing backup"
 vagrant ssh -c "cd /var/www/durhamatletico; pv database.sql | $(drush sql-connect)"
