e10d73a2fd73772cc86a11ac3a8155899b6069aa
Kosta Harlan <kosta@kostaharlan.net>
2016-07-18 22:09:17 -0400

    Use nid as parameter

--- modules/custom/durhamatletico_api/src/Plugin/rest/resource/TournamentRestResource.php
+++ modules/custom/durhamatletico_api/src/Plugin/rest/resource/TournamentRestResource.php
@@ -2,14 +2,11 @@
 
 namespace Drupal\durhamatletico_api\Plugin\rest\resource;
 
-use Drupal\Core\Session\AccountProxyInterface;
 use Drupal\rest\Plugin\ResourceBase;
 use Drupal\rest\ResourceResponse;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Psr\Log\LoggerInterface;
+use Drupal\node\Entity\Node;
 
 /**
  * Provides a resource to get view modes by entity and bundle.
@@ -18,7 +15,7 @@ use Psr\Log\LoggerInterface;
  *   id = "tournament_rest_resource",
  *   label = @Translation("Tournaments for bracket display"),
  *   uri_paths = {
- *     "canonical" = "/api/tournaments/{id}"
+ *     "canonical" = "/api/tournaments/{nid}"
  *   }
  * )
  */
@@ -29,24 +26,26 @@ class TournamentRestResource extends ResourceBase {
    *
    * Returns formatted output for use with bracket JS.
    *
-   * @param string $id
-   *   The path of the tournament, e.g. node/1655.
+   * @param int $nid
+   *   The node ID of the tournament, e.g. 1655.
    *
    * @return \Drupal\rest\ResourceResponse
    *   The response containing the log entry.
-   *j
+   *
    * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
    *   Thrown when the tournament entry was not found.
    * @throws \Symfony\Component\HttpKernel\Exception\BadRequestHttpException
    *   Thrown when no tournament entry was provided.
    */
-  public function get($id = NULL) {
-    if ($id) {
-      if (!$id) {
+  public function get($nid = NULL) {
+    if ($nid) {
+      // Look up node.
+      $node = Node::load($nid);
+      if (!$node || $node->getType() !== 'league') {
         throw new NotFoundHttpException(t('Tournament not found.'));
       }
 
-      return new ResourceResponse('test');
+      return new ResourceResponse($node);
     }
 
     throw new BadRequestHttpException(t('No tournament ID was provided.'));
