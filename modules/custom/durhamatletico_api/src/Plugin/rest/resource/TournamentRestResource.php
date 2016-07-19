498059779ed6016a4055c591591312f34007732c
Kosta Harlan <kosta@kostaharlan.net>
2016-07-18 22:49:13 -0400

    Initial implementation

--- modules/custom/durhamatletico_api/src/Plugin/rest/resource/TournamentRestResource.php
+++ modules/custom/durhamatletico_api/src/Plugin/rest/resource/TournamentRestResource.php
@@ -45,7 +45,52 @@ class TournamentRestResource extends ResourceBase {
         throw new NotFoundHttpException(t('Tournament not found.'));
       }
 
-      return new ResourceResponse($node);
+      // Load all game referencing this competition.
+      $query = \Drupal::entityQuery('node');
+      $query->condition('type', 'game');
+      $query->condition('field_division', $nid);
+      $ids = $query->execute();
+
+      if (!count($ids)) {
+        throw new NotFoundHttpException(t('Unable to load games.'));
+      }
+
+      // Load games and results.
+      $initial_teams = $games = [];
+      $round_one = $round_two = $round_three = [];
+      foreach ($ids as $game_nid) {
+        $game_node = Node::load($game_nid);
+        // Load team.
+        $home_team_node = Node::load($game_node->get('field_home_team')->entity->id());
+        $away_team_node = Node::load($game_node->get('field_away_team')->entity->id());
+        switch ($game_node->field_cup_round->value) {
+          case 1:
+            $initial_teams[] = [
+              $home_team_node->field_abbreviation->value,
+              $away_team_node->field_abbreviation->value,
+            ];
+            // Round one results.
+            $round_one[] = [$game_node->field_home_team_score->value, $game_node->field_away_team_score->value];
+            break;
+
+          case 2:
+            if ($game_node->field_bracket_grouping->value == 1) {
+              $round_two[] = [$game_node->field_home_team_score->value, $game_node->field_away_team_score->value];
+            }
+            break;
+
+          case 3:
+            $round_three[] = [$game_node->field_home_team_score->value, $game_node->field_away_team_score->value];
+            break;
+        }
+      }
+      $response['teams'] = $initial_teams;
+      $response['results'] = [
+        $round_one,
+        $round_two,
+        $round_three,
+      ];
+      return new ResourceResponse($response);
     }
 
     throw new BadRequestHttpException(t('No tournament ID was provided.'));
