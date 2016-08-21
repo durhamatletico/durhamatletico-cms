612860ee1079a029a6965fa3f6ce636720a20750
Kosta Harlan <kosta@kostaharlan.net>
2016-08-21 19:55:51 -0400

    Add Travis CI build

--- tests/scripts/build.sh
+++ tests/scripts/build.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-set -e
-
-# Get Pantheon's Command Line Tool, terminus.
-sudo curl https://github.com/pantheon-systems/cli/releases/download/0.11.1/terminus.phar -L -o /usr/local/bin/terminus && sudo chmod +x /usr/local/bin/terminus
-
 # Log into terminus.
-terminus auth login $PANTHEON_EMAIL --password=$PANTHEON_PASSWORD
+docker-compose exec php terminus auth login $PANTHEON_EMAIL --password=$PANTHEON_PASSWORD
 
 echo "Creating backup"
-terminus site backups create --element=database --site=durham-atletico --env=live
+docker-compose exec php terminus site backups create --element=database --site=durham-atletico --env=live
 echo "Downloading backup"
-terminus site backups get --element=db --site=durham-atletico --env=live --to=database.sql.gz --latest
+docker-compose exec php terminus site backups get --element=db --site=durham-atletico --env=live --to=database.sql.gz --latest
 rm database.sql
 echo "y" | gunzip database.sql.gz
 echo "Importing backup"
