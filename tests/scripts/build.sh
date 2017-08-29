2a4a5ed7254abdbcb5bee695ac1c8ad748018a77
Kosta Harlan <kosta@fastmail.com>
2017-08-29 13:04:55 -0400

    Use terminus docker image

--- tests/scripts/build.sh
+++ tests/scripts/build.sh
@@ -2,13 +2,15 @@
 
 set -ex
 # Log into terminus.
-docker-compose exec php terminus auth login $PANTHEON_EMAIL --password=$PANTHEON_PASSWORD
+docker volume create --name=durhamatletico_terminus_data
+docker-compose run --rm --entrypoint="sh -c" terminus "mkdir -p /terminus/cache/tokens"
+docker-compose run --rm terminus auth:login --machine-token=$PANTHEON_TOKEN
+docker-compose run --rm terminus site:info durham-atletico
 
 echo "Creating backup"
-docker-compose exec php terminus site backups create --element=database --site=durham-atletico --env=live
+docker-compose exec php terminus site backup:create durham-atletico.live -n --element=db --keep-for=1
 echo "Downloading backup"
-docker-compose exec php terminus site backups get --element=db --site=durham-atletico --env=live --to=database.sql.gz --latest
-rm database.sql
+docker-compose exec php terminus site backup:get durham-atletico.live -n --element=db --to=database.sql.gz
 echo "y" | gunzip database.sql.gz
 echo "Importing backup"
 docker-compose exec -T mariadb mysql -uroot -proot durhamatletico_docker < database.sql
