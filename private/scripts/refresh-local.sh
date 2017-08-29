67b17c083e7062e87ad538ace59bacb9dd5f229e
Kosta Harlan <kosta@fastmail.com>
2017-08-28 21:34:30 -0400

    Switch to wodby stack

--- private/scripts/refresh-local.sh
+++ private/scripts/refresh-local.sh
@@ -9,7 +9,7 @@ DB_URL=$(terminus backup:get --element=db durham-atletico.live)
 curl -L -o database.sql.gz $DB_URL
 rm database.sql
 # Drop local DB
-docker exec -i durhamatletico_php drush sql-drop -y
+docker exec -i durhamatletico_php_1 drush sql-drop -y
 echo "y" | gunzip database.sql.gz
 echo "Importing backup"
-pv database.sql | docker exec -i durhamatletico_db mysql -uroot -proot durhamatletico_docker
+pv database.sql | docker exec -i durhamatletico_mariadb_1 mysql -uroot -proot durhamatletico_docker
