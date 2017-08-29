a37cf9cfa51586132035deb2222ac4e2e191e76a
Kosta Harlan <kosta@fastmail.com>
2017-08-28 20:40:29 -0400

    Fix curl command

--- private/scripts/refresh-local.sh
+++ private/scripts/refresh-local.sh
@@ -6,7 +6,7 @@ echo "Creating backup"
 terminus backup:create --element=database durham-atletico.live
 echo "Downloading backup"
 DB_URL=$(terminus backup:get --element=db durham-atletico.live)
-curl -L -o $DB_URL database.sql.gz
+curl -L -o database.sql.gz $DB_URL
 rm database.sql
 # Drop local DB
 docker exec -i durhamatletico_php drush sql-drop -y
