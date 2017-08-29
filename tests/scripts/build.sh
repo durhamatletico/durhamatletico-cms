c586ac87dddcbf3bd81a3d909cf56711e507f7c7
Kosta Harlan <kosta@fastmail.com>
2017-08-29 14:07:21 -0400

    Update terminus commands

--- tests/scripts/build.sh
+++ tests/scripts/build.sh
@@ -8,9 +8,9 @@ docker-compose run --rm terminus auth:login --machine-token=$PANTHEON_TOKEN
 docker-compose run --rm terminus site:info durham-atletico
 
 echo "Creating backup"
-docker-compose exec php terminus site backup:create durham-atletico.live -n --element=db --keep-for=1
+docker-compose run --rm terminus backup:create durham-atletico.live -n --element=db --keep-for=1
 echo "Downloading backup"
-docker-compose exec php terminus site backup:get durham-atletico.live -n --element=db --to=database.sql.gz
+docker-compose run --rm terminus backup:get durham-atletico.live -n --element=db --to=database.sql.gz
 echo "y" | gunzip database.sql.gz
 echo "Importing backup"
 docker-compose exec -T mariadb mysql -uroot -proot durhamatletico_docker < database.sql
