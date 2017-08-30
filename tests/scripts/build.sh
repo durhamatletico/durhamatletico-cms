a5c3d26b6248d348d4f09ca4facfcb839f357d61
Kosta Harlan <kosta@fastmail.com>
2017-08-30 17:04:57 -0400

    debug info

--- tests/scripts/build.sh
+++ tests/scripts/build.sh
@@ -12,6 +12,8 @@ docker-compose run --rm terminus backup:create durham-atletico.live -n --element
 echo "Downloading backup"
 mkdir -p tmp
 docker-compose run --rm terminus backup:get durham-atletico.live -n --element=db --to=tmp/database.sql.gz
+ls -la
+whoami
 echo "y" | gunzip tmp/database.sql.gz
 echo "Importing backup"
 docker-compose exec -T mariadb mysql -uroot -proot durhamatletico_docker < tmp/database.sql
