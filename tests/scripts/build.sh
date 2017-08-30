2fc154fb0ad0e8b793b066c21e316e25aa3407d7
Kosta Harlan <kosta@fastmail.com>
2017-08-30 16:45:18 -0400

    Create tmp dir for db

--- tests/scripts/build.sh
+++ tests/scripts/build.sh
@@ -10,10 +10,11 @@ docker-compose run --rm terminus site:info durham-atletico
 echo "Creating backup"
 docker-compose run --rm terminus backup:create durham-atletico.live -n --element=db --keep-for=1
 echo "Downloading backup"
-docker-compose run --rm terminus backup:get durham-atletico.live -n --element=db --to=database.sql.gz
-echo "y" | gunzip database.sql.gz
+mkdir -p tmp
+docker-compose run --rm terminus backup:get durham-atletico.live -n --element=db --to=tmp/database.sql.gz
+echo "y" | gunzip tmp/database.sql.gz
 echo "Importing backup"
-docker-compose exec -T mariadb mysql -uroot -proot durhamatletico_docker < database.sql
+docker-compose exec -T mariadb mysql -uroot -proot durhamatletico_docker < tmp/database.sql
 
 # Clear cache
 docker-compose exec php drush cr -yv
