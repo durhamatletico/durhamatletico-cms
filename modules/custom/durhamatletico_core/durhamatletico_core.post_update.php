511cf5b22404b88dd0007764b64b2e25209f2744
Kosta Harlan <kosta@fastmail.com>
2017-09-05 21:48:15 -0400

    Adjust views for new field, add update hook, fix view OR query

---
+++ modules/custom/durhamatletico_core/durhamatletico_core.post_update.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Migrate HTML color field to new Color Field.
+ */
+function durhamatletico_core_post_update_color_field_migration() {
+  $query = \Drupal::entityQuery('node')
+      ->condition('status', 1)
+      ->condition('type', 'team');
+  $nids = $query->execute();
+  foreach ($nids as $nid) {
+    $team_node = \Drupal\node\Entity\Node::load($nid);
+    $legacy_color = $team_node->get('field_jersey_color_html_')->getString();
+    $team_node->set('field_jersey_html_color', [
+      'color' => $legacy_color,
+      'opacity' => NULL,
+    ]);
+    $team_node->save();
+    \Drupal::logger('durhamatletico_core')->info(t('Migrating jersey color to new field for node %nid', ['%nid' => $nid]));
+  }
+}
