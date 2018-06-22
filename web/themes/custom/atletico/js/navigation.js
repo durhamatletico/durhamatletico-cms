db9ba97f3c1bb34fe0e5392bd076ea34ed7cd4cb
Anne Tomasevich <annetvich@gmail.com>
2018-06-21 20:53:50 -0400

    Style header items

--- web/themes/custom/atletico/js/navigation.js
+++ web/themes/custom/atletico/js/navigation.js
@@ -1,14 +1,15 @@
 (function ($) {
-  const menuToggle = $('#js-mobile-menu-button').unbind();
+  const $menuToggle = $('#js-mobile-menu-button').unbind();
   const $mainNav = $('#js-mobile-menu');
   $mainNav.removeClass('show');
 
-  menuToggle.on('click', function(e) {
+  $menuToggle.on('click', function (e) {
     e.preventDefault();
-    $mainNav.slideToggle(function(){
-      if($mainNav.is(':hidden')) {
+    $mainNav.slideToggle(function (){
+      if ($mainNav.is(':hidden')) {
         $mainNav.removeAttr('style');
       }
     });
+    $menuToggle.toggleClass('open');
   });
 })(jQuery);
