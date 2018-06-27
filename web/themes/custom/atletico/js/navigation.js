9598c188732de0970dcb1e9f295b1eb170afa211
Anne Tomasevich <annetvich@gmail.com>
2018-06-26 21:46:52 -0400

    Add mobile menu close button

--- web/themes/custom/atletico/js/navigation.js
+++ web/themes/custom/atletico/js/navigation.js
@@ -1,9 +1,19 @@
 (function ($) {
   const $menuToggle = $('#js-mobile-menu-button').unbind();
+  const $menuClose = $('#js-mobile-menu-close').unbind();
   const $mainNav = $('#js-mobile-menu');
   $mainNav.removeClass('show');
 
   $menuToggle.on('click', function (e) {
+    toggleMenu(e);
+  });
+
+  $menuClose.on('click', function (e) {
+    toggleMenu(e);
+  });
+
+  // Slide the main menu open or closed.
+  function toggleMenu(e) {
     e.preventDefault();
     $mainNav.slideToggle(function (){
       if ($mainNav.is(':hidden')) {
@@ -11,5 +21,7 @@
       }
     });
     $menuToggle.toggleClass('open');
-  });
+  }
+
+
 })(jQuery);
